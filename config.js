16c0b2f14e4938d61f2b05a81e35ce3408148b84
giriadabola <giriadabola@gmail.com>
2025-08-23 17:22:11 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8WcFD7jC55feYYqdY7aJSgxXyXkEjTX0",
+  authDomain: "g-games-8a8fc.firebaseapp.com",
+  projectId: "g-games-8a8fc",
+  storageBucket: "g-games-8a8fc.appspot.com",
+  messagingSenderId: "689897349449",
+  appId: "1:689897349449:web:536599794579901beb7a98",
+  measurementId: "G-GTTPJ6G5MD"
+};
