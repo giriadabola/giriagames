baea08b1f8d3db945fc4dca896f3309c845010da
giriadabola <giriadabola@gmail.com>
2026-08-31 09:59:32 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8WcFD7jC55feYYqdY7aJSgxXyXkEjTX0",
+    authDomain: "g-games-8a8fc.firebaseapp.com",
+    projectId: "g-games-8a8fc",
+    storageBucket: "g-games-8a8fc.appspot.com",
+    messagingSenderId: "689897349449",
+    appId: "1:689897349449:web:536599794579901beb7a98",
+    measurementId: "G-GTTPJ6G5MD"
+};
+
+if (typeof window !== 'undefined') {
+    window.firebaseConfig = firebaseConfig;
+}
